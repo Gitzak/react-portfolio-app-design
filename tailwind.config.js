0d3f32f04631286a7dbd63d33d72735448a5f1bc
GitZak <zakaria.belrhali@gmail.com>
2024-10-14 18:27:03 +0100

    modif tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,12 +22,6 @@ export default {
         outfit: ["Outfit", "sans-serif"],
         RobotoMono: ["Roboto Mono", "monospace"],
       },
-      content: {
-        brush: "url('./assets/brush.png')",
-        person1: "url('./assets/person-1.png')",
-        person2: "url('./assets/person-2.png')",
-        person3: "url('./assets/person-3.png')",
-      },
     },
     screens: {
       xs: "480px",
